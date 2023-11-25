4c0f945e76bd29d2006f204cf102377ea8c1bcfb
cernochmartin <cernochm@outlook.cz>
2023-11-25 10:42:36 +0100

    feat: add mysql database

---
+++ database.js
@@ -0,0 +1,18 @@
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const connection = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+}).promise()
+
+async function getUsers() {
+    const [rows] = await connection.query('SELECT * FROM users')
+    return rows
+}
+
+const users = await getUsers()
+console.log(users)
